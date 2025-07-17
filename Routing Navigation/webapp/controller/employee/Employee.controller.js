6f13206f2221209a4571587b82cac5a44d05829e
Raaphaaa <raphajan@freenet.de>
2025-07-17 16:33:48 +0200

    Navigation & Routing Step 7: Navigate to Routes with Mandatory Parameters

---
+++ Routing Navigation/webapp/controller/employee/Employee.controller.js
@@ -0,0 +1,41 @@
+sap.ui.define(
+  ["sap/ui/demo/nav/controller/BaseController"],
+  function (BaseController) {
+    "use strict";
+
+    return BaseController.extend(
+      "sap.ui.demo.nav.controller.employee.Employee",
+      {
+        onInit: function () {
+          let oRouter = this.getRouter();
+          oRouter
+            .getRoute("employee")
+            .attachMatched(this._onRouteMatched, this);
+        },
+        _onRouteMatched: function (oEvent) {
+          let oArgs, oView;
+          oArgs = oEvent.getParameter("arguments");
+          oView = this.getView();
+
+          oView.bindElement({
+            path: "/Employees(" + oArgs.employeeId + ")",
+            eventy: {
+              change: this._onBindingChange.bind(this),
+              dataRequested: function (oEvent) {
+                oView.setBusy(true);
+              },
+              dataReceived: function (oEvent) {
+                oView.setBusy(false);
+              },
+            },
+          });
+        },
+        _onBindingChange: function (oEvent) {
+          if (!this.getView().getBindingContext()) {
+            this.getRouter().getTargets().display("notFound");
+          }
+        },
+      }
+    );
+  }
+);
--- Routing Navigation/webapp/controller/employee/EmployeeList.controller.js
+++ Routing Navigation/webapp/controller/employee/EmployeeList.controller.js
@@ -5,7 +5,16 @@ sap.ui.define(
 
     return BaseController.extend(
       "sap.ui.demo.nav.controller.employee.EmployeeList",
-      {}
+      {
+        onListItemPressed: function (oEvent) {
+          let oItem, oContext;
+          oItem = oEvent.getSource();
+          oContext = oItem.getBindingContext();
+          this.getRouter().navTo("employee", {
+            employeeId: oContext.getProperty("EmployeeID"),
+          });
+        },
+      }
     );
   }
 );
