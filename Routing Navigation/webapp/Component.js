a71d77e52d65cb1e56163046475f81da1eb1643b
Raaphaaa <raphajan@freenet.de>
2025-07-17 10:30:34 +0200

    Navigation & Routing Step 2

--- Routing Navigation/webapp/Component.js
+++ Routing Navigation/webapp/Component.js
@@ -1,14 +1,17 @@
-sap.ui.define([
-	"sap/ui/core/UIComponent"
-], function (UIComponent) {
-	"use strict";
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  "use strict";
 
-	return UIComponent.extend("sap.ui.demo.nav.Component", {
+  return UIComponent.extend("sap.ui.demo.nav.Component", {
+    metadata: {
+      manifest: "json",
+    },
 
-		metadata: {
-			manifest: "json"
-		}
+    // Override of the default init function
+    init: function () {
+      // calling the parent's init function
+      UIComponent.prototype.init.apply(this, arguments);
 
-	});
-
+      this.getRouter().initialize();
+    },
+  });
 });
