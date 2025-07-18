2de338c2b19841728badb5073bf3b40b4492dc6b
Raaphaaa <raphajan@freenet.de>
2025-07-18 11:03:23 +0200

    Navigation & Routing Step 9: Allow Bookmarkable Tabs with Optional Query Parameters

--- Routing Navigation/webapp/controller/employee/Resume.controller.js
+++ Routing Navigation/webapp/controller/employee/Resume.controller.js
@@ -1,20 +1,24 @@
 sap.ui.define(
-  ["sap/ui/demo/nav/controller/BaseController"],
-  function (BaseController) {
+  ["sap/ui/demo/nav/controller/BaseController", "sap/ui/model/json/JSONModel"],
+  function (BaseController, JSONModel) {
     "use strict";
+
+    let _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 
     return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
       onInit: function () {
         let oRouter = this.getRouter();
+        this.getView().setModel(new JSONModel(), "view");
         oRouter
           .getRoute("employeeResume")
           .attachMatched(this._onRouteMatched, this);
       },
 
       _onRouteMatched: function (oEvent) {
-        let oArgs, oView;
+        let oArgs, oView, oQuery;
         oArgs = oEvent.getParameter("arguments");
         oView = this.getView();
+
         oView.bindElement({
           path: "/Employees(" + oArgs.employeeId + ")",
           events: {
@@ -27,12 +31,41 @@ sap.ui.define(
             },
           },
         });
+
+        oQuery = oArgs["?query"];
+        if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1) {
+          oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+        } else {
+          this.getRouter().navTo(
+            "employeeResume",
+            {
+              employeeId: oArgs.employeeId,
+              "?query": {
+                tab: _aValidTabKeys[0],
+              },
+            },
+            true
+          );
+        }
       },
 
       _onBindingChange: function (oEvent) {
         if (!this.getView().getBindingContext()) {
           this.getRouter().getTargets().display("notFound");
         }
+      },
+      onTabSelect: function (oEvent) {
+        let oContext = this.getView().getBindingContext();
+        this.getRouter().navTo(
+          "employeeResume",
+          {
+            employeeId: oContext.getProperty("EmployeeID"),
+            "?query": {
+              tab: oEvent.getParameter("selectedKey"),
+            },
+          },
+          true /*without history*/
+        );
       },
     });
   }
